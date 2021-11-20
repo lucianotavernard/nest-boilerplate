66d0102f9d421beff06c4971b3f465dafd6ccf00
Luciano Tavernard <lucianotavernard@gmail.com>
2021-11-20 00:12:39 -0300

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './shared/app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  await app.listen(3000);
+}
+
+bootstrap();
